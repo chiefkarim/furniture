493054de57ed86c2149530896c4d01f6d5371fae
chiefkarim <krimotop2016@gmail.com>
2025-06-08 10:49:49 +0100

    feat: remove padding from first child in trends section carousel

--- components/Trends.vue
+++ components/Trends.vue
@@ -42,7 +42,7 @@
               :items="inspirationImages"
               :slidesToScroll="2"
               :ui="{
-                item: 'basis-[40%] ps-[30px]',
+                item: 'basis-[40%] [&:not(:first-child)]:pxs-[30px]',
               }"
               class="inspiration-image-wrapper"
             >
